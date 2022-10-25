21c39e4363af22f128b52ebec618c5f6f2a6e450
rjarvinen <riku.jarvinen@fmi.fi>
2022-10-25 17:13:09 +0300

    An optional config file option "data_save_during_first_timesteps" to choose if output files are saved during N first time step, where N is the value of the option. Default value: 0.

--- src/kernel/main.cpp
+++ src/kernel/main.cpp
@@ -168,6 +168,7 @@ int main(int argn,char* args[]) {
    objectWrapper.configReader.add("Simulation.data_save_interval_unit","Unit in which 'data_save_interval' is given (time/timestep) (string).",string(""));
    objectWrapper.configReader.add("Simulation.data_save_interval","Interval between data saves (int or float).",NaN);
    objectWrapper.configReader.add("Simulation.data_save_initial_state","Save initial state (yes or no).",string("yes"));
+   objectWrapper.configReader.add("Simulation.data_save_during_first_timesteps","Save state during first N timesteps (int).",0);
    objectWrapper.configReader.add("Simulation.restart_write_interval","Interval, in time steps, between restart file writes. Defaults to zero value, i.e. restart files are not written (int).",0);
    objectWrapper.configReader.add("Simulation.random_number_generator.seed","Seed value for random number generator, with value zero it is calculated from system clock (int).",(int)0);
    objectWrapper.configReader.addComposed("LoadBalance.methods","Load balancing method for each hierarchical level (NONE / BLOCK / RANDOM / RCB / RIB / GRAPH / HYPERGRAPH).");
@@ -399,9 +400,13 @@ int main(int argn,char* args[]) {
       }
    }
 
-   // Read if output file should be save after initialization
+   // Read if output files should be saved after initialization
    string dataSaveInitial = "yes";
    objectWrapper.configReader.get("Simulation.data_save_initial_state",dataSaveInitial);
+
+   // Read if output files should be saved during first N timesteps
+   int dataSaveFirstTimesteps = 0;
+   objectWrapper.configReader.get("Simulation.data_save_during_first_timesteps",dataSaveFirstTimesteps);
 
    // Read if mesh should be written to every VLSV file:
    string meshAlwaysWritten;
@@ -520,6 +525,9 @@ int main(int argn,char* args[]) {
                objectWrapper.sim.atDataSaveStep = true;
             }
          }
+	 if (objectWrapper.sim.timestep < dataSaveFirstTimesteps) {
+	    objectWrapper.sim.atDataSaveStep = true;
+	 }
          
          // Check if restart file should be written:
          if (objectWrapper.sim.restartWriteInterval > 0 && objectWrapper.sim.timestep != objectWrapper.sim.restartTimestep) {
